bc054f0db5cc2d8ca1e3b504233cbf47ad8de6ba
unknown <Tangjiayu-dong>
2019-12-02 12:07:57 +0800

    this is a test

---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <div class="home-one">
+      <dl>
+        <dd>
+
+        </dd>
+        <dt>
+          <h2>我是标题</h2>
+          <p></p>
+        </dt>
+      </dl>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
